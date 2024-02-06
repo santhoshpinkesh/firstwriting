cf1c79ec1d86d6636dadf0d64ed4fcab48a23036
santhoshpinkesh <158996813+santhoshpinkesh@users.noreply.github.com>
2024-02-06 07:52:43 +0000

    updated route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+  return render_template("home.html")
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+
+@app.route("/test")
+def test():
+  return "test is working"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
